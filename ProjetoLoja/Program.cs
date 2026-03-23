f0ce887ffced1e8047c50e5e8a6674682bf722dd
Wellington <welltecnic@hotmail.com>
2026-03-23 19:30:51 -0300

    chore:Criando e Configurando o projetoLoja

---
+++ ProjetoLoja/Program.cs
@@ -0,0 +1,37 @@
+
+
+using ProjetoLoja.Interfaces;
+using ProjetoLoja.Repositorio;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+//INJEÇÃO DE DEPENDÊNCIA
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
+
+
+
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
